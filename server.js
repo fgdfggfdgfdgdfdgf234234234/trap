7e7b9297b55094863e9536535c2dc526c8230ce2
fgdfggfdgfdgdfdgf234234234 <disant2600@gmail.com>
2025-03-12 18:16:00 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-// Додаємо CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://vaniafotkiloh.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST');
@@ -12,20 +11,20 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-// GET-маршрут для кореня
 app.get('/', (req, res) => {
   res.send('Сервер працює! Перевір /log-ip через POST-запит.');
 });
 
-// POST-маршрут для логування IP
 app.post('/log-ip', (req, res) => {
+  console.log('Отримано запит на /log-ip:', req.body); // Логування вхідного запиту
   const ip = req.body.ip || 'Unknown IP';
   const logEntry = `${new Date().toISOString()} - IP: ${ip}\n`;
   fs.appendFile('ip_logs.txt', logEntry, (err) => {
     if (err) {
-      console.error('Помилка запису:', err);
+      console.error('Помилка запису:', err); // Логування помилки
       return res.status(500).send('Помилка сервера');
     }
+    console.log('IP записано:', logEntry); // Логування успішного запису
     res.send('IP записано!');
   });
 });
